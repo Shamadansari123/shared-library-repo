bdab5450f127e83754e3144ed030552c69a4ba4e
Shamadansari123 <100352782+Shamadansari123@users.noreply.github.com>
2025-07-01 20:22:34 +0530

    Update helloworld.groovy

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -3,5 +3,9 @@ def call(name){
 }
 
 def mul(x,y){
-   echo "multiplication of ${x} and ${y} is ${x+y}"
+   echo "multiplication of ${x} and ${y} is ${x*y}"
+}
+
+def add(x,y){
+   echo "addition of ${x} and ${y} is: ${x+y} "
 }
